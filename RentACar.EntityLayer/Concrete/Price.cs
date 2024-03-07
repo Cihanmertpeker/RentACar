49203981f978bf61f817f4825355b8b7e48767c8
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-03-07 12:24:15 +0300

    Add project files.

---
+++ RentACar.EntityLayer/Concrete/Price.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RentACar.EntityLayer.Concrete
+{
+    public class Price
+    {
+        public int PriceID { get; set; }
+        public int CarID { get; set; }
+        public Car Car { get; set; }
+        public decimal PriceValue { get; set; }
+    }
+}
