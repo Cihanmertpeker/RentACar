7b5410a1c191283868612238441477fda86c153c
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-03-08 15:44:30 +0300

    Data Access Layer Completed

--- RentACar.DataAccessLayer/Concrete/RentACarContext.cs
+++ RentACar.DataAccessLayer/Concrete/RentACarContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using RentACar.EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,18 @@ using System.Threading.Tasks;
 
 namespace RentACar.DataAccessLayer.Concrete
 {
-    public class RentACarContext
+    public class RentACarContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.\\SQLExpress; initial catalog=RentACarDB; Integrated Security=true;");
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<CarCategory> CarCategories { get; set; }
+        public DbSet<CarStatus> CarStatuses { get; set; }
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<Price> Prices { get; set; }
+
     }
 }
