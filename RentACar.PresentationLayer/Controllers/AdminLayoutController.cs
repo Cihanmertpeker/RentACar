78d68736f3f59928061eed4ea51bde87b9d28396
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-03-24 20:53:55 +0300

    Admin Panel Completed

---
+++ RentACar.PresentationLayer/Controllers/AdminLayoutController.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.BusinessLayer.Abstract;
+
+namespace RentACar.PresentationLayer.Controllers
+{
+    public class AdminLayoutController : Controller
+    {       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ RentACar.PresentationLayer/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using RentACar.EntityLayer.Concrete;
+using RentACar.PresentationLayer.Models;
+
+namespace RentACar.PresentationLayer.Controllers
+{
+    [AllowAnonymous]
+    [Route("[controller]/[action]")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginViewModel model)
+        {
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, false, false);
+
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
+            }
+            else
+            {
+                ModelState.AddModelError("", "Kullanıcı adı veya şifre yanlış.");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
